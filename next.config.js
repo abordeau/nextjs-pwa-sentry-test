01667749d04e166d80f3e8d4af8cc3c8f831559e
Antoine Bordeau <bordeau.antoine@gmail.com>
2021-05-12 19:18:30 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const { withSentryConfig } = require('@sentry/nextjs')
 
-module.exports = withPWA({
+const moduleExports = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
   },
 })
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+// This not work
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
+
+// This work
+// module.exports = moduleExports
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -14,33 +14,11 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <button type="button" onClick={() => {
+            throw new Error('Sentry Frontend Error')
+          }}>
+            Throw error
+          </button>
         </div>
       </main>
 
@@ -51,7 +29,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
         </a>
       </footer>
     </div>
